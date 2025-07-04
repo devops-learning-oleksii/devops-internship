0dd0d7c6729b69d6144c848f11cec41573ac89f7
NightPhoenixSC2 <Oleksiy.Zaharchyk@gmail.com>
2025-07-04 23:37:19 +0300

    restructured milestone2

--- milestone2/terraform1/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "aws--terraform-bucket"
-    key          = "AWS/terraform-states/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
