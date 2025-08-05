4e04e821aed6c0a18a552ef6785b9f3e5e8e60b2
NightPhoenixSC2 <Oleksiy.Zaharchyk@gmail.com>
2025-08-05 12:51:42 +0300

    Add RDS. Add Provision to server node

--- Schedule-AWS/terraform/infra/main.tf
+++ Schedule-AWS/terraform/infra/main.tf
@@ -4,41 +4,54 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = "devops-schedule-secret" # Name of you secret
+}
+
 locals {
   config            = jsondecode(file("variables.json"))
   global            = local.config.global
   terraform_config  = local.config.terraform
 
+  db_creds = jsondecode(
+    data.aws_secretsmanager_secret_version.creds.secret_string
+  )
+
   ec2_instances_flat = merge([
     for inst_name, inst_conf in local.terraform_config.instances.ec2_instances : {
-      for i in range(lookup(inst_conf, "count", 1)) : 
+      for i in range(lookup(inst_conf, "count", 1)) :
         "${inst_name}-${i + 1}" => merge(inst_conf, { index = i + 1, base_name = inst_name })
     }
   ]...)
 }
 
 module "vpc" {
-    source = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
 
-    name = "${local.global.environment}-vpc"
-    
-    cidr = local.terraform_config.vpc.cidr
+  name = "${local.global.environment}-vpc"
 
-    azs = local.terraform_config.vpc.availability_zones
-    private_subnets = local.terraform_config.vpc.private_subnet_cidrs
-    public_subnets =local.terraform_config.vpc.public_subnet_cidrs
+  cidr = local.terraform_config.vpc.cidr
 
-    enable_nat_gateway = true
-    single_nat_gateway = true
+  azs = local.terraform_config.vpc.availability_zones
+  private_subnets = local.terraform_config.vpc.private_subnet_cidrs
+  public_subnets =local.terraform_config.vpc.public_subnet_cidrs
+  database_subnets = local.terraform_config.vpc.database_subnets
 
-    enable_dns_hostnames = true
-    enable_dns_support = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
-    tags = merge(
-        {
-        Name = "${local.global.environment}-vpc"
-        },
-    local.terraform_config.tags
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  # Database
+  create_database_subnet_group       = true
+  create_database_subnet_route_table = true
+
+  tags = merge(
+      {
+      Name = "${local.global.environment}-vpc"
+      },
+  local.terraform_config.tags
   )
 }
 
@@ -84,7 +97,7 @@ module "security_groups" {
       Name = each.key
       },
       local.terraform_config.tags
-  )    
+  )
 }
 
 module "ec2_instance" {
@@ -104,6 +117,8 @@ module "ec2_instance" {
   )
 
   associate_public_ip_address = each.value.public_ip
+
+  user_data = (each.value.user_data != null && each.value.user_data != "") ? file("${path.module}/user_data/${each.value.user_data}") : null
 
   tags = merge(
       {
@@ -131,10 +146,50 @@ module "ssh_config" {
   depends_on = [module.ec2_instance]
 }
 
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+  identifier = "${local.global.environment}-db"
+
+  engine = "postgres"
+  engine_version = "15.13"
+  instance_class = local.terraform_config.database.instance_class
+  allocated_storage = local.terraform_config.database.allocated_storage
+  family = "postgres15"
+
+  db_name  = local.db_creds.db_name
+  password = local.db_creds.db_password
+  username = local.db_creds.db_username
+  port = 5432
+
+  vpc_security_group_ids = [module.security_groups["database_sg"].security_group_id]
+  db_subnet_group_name = module.vpc.database_subnet_group_name
+
+  multi_az            = false
+  publicly_accessible = false
+  skip_final_snapshot = true
+  deletion_protection = false
+
+  manage_master_user_password = false
+
+  parameters = [
+    {
+      name  = "rds.force_ssl"
+      value = "0"
+    }
+  ]
+
+  tags = merge(
+    {
+      Name = "${local.global.environment}-db"
+    },
+    local.terraform_config.tags
+  )
+}
+
 # Use aws_route53_zone resource to create zone or just copy your zone id to aws_route53_record resource so NS will be static
 
 # resource "aws_route53_zone" "main" {
-#   name = local.terraform_config.domain_name 
+#   name = local.terraform_config.domain_name
 # }
 
 
@@ -148,3 +203,4 @@ resource "aws_route53_record" "a_records" {
   ttl     = 300
   records = [module.ec2_instance["server-1"].public_ip]
 }
+
--- Schedule-AWS/terraform/infra/outputs.tf
+++ Schedule-AWS/terraform/infra/outputs.tf
@@ -7,3 +7,9 @@ output "worker_1_public_ip" {
 output "worker_2_public_ip" {
   value = module.ec2_instance["worker-2"].private_ip
 }
+output "worker_3_public_ip" {
+  value = module.ec2_instance["worker-3"].private_ip
+}
+output "DATABASE_ENDPOINT" {
+  value = module.db.db_instance_address
+}
